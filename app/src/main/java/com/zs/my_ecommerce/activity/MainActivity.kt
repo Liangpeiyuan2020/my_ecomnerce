dea95afa5ec574995d0a69238a5d12f064d813bc
梁培源 <liangpeiyuan@yingzi.com>
2025-09-15 15:51:04 +0800

    mainActivity

--- app/src/main/java/com/zs/my_ecommerce/activity/MainActivity.kt
+++ app/src/main/java/com/zs/my_ecommerce/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zs.my_ecommerce
+package com.zs.my_ecommerce.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.zs.my_ecommerce.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
