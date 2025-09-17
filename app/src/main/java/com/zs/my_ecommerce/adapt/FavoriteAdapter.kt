ed23cb2ea79f5f52ae92e5ae21bc7f556afaa141
梁培源 <liangpeiyuan@yingzi.com>
2025-09-17 14:02:29 +0800

    favorite

---
+++ app/src/main/java/com/zs/my_ecommerce/adapt/FavoriteAdapter.kt
@@ -0,0 +1,39 @@
+package com.zs.my_ecommerce.adapt
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zs.my_ecommerce.bean.Favorite
+import com.zs.my_ecommerce.databinding.ItemFavoriteBinding
+
+class FavoriteAdapter(
+    val favorites: List<Favorite>,
+    onItemClick: (Favorite) -> Unit,
+    onAddToCartClick: (Favorite) -> Unit,
+    onDeleteClick: (Favorite) -> Unit
+) : RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FavoriteViewHolder {
+        val binding =
+            ItemFavoriteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FavoriteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: FavoriteViewHolder,
+        position: Int
+    ) {
+        holder.bind(favorites[position])
+    }
+
+    override fun getItemCount() = favorites.size
+
+    inner class FavoriteViewHolder(val binding: ItemFavoriteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(favorite: Favorite) {
+            binding.favorite = favorite
+        }
+    }
+}
--- app/src/main/java/com/zs/my_ecommerce/fragment/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/zs/my_ecommerce/fragment/favorite/FavoriteFragment.kt
@@ -6,26 +6,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zs.my_ecommerce.R
+import com.zs.my_ecommerce.activity.MainViewModel
+import com.zs.my_ecommerce.activity.MainViewModelFactory
+import com.zs.my_ecommerce.adapt.FavoriteAdapter
+import com.zs.my_ecommerce.bean.Favorite
+import com.zs.my_ecommerce.dataBase.MyDataBase
+import com.zs.my_ecommerce.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+    private lateinit var binding: FragmentFavoriteBinding
+    private val favoriteVm: FavoriteViewModel by viewModels()
+    private val mainVm: MainViewModel by activityViewModels {
+        MainViewModelFactory(MyDataBase.getInstance())
+    }
 
     companion object {
         fun newInstance() = FavoriteFragment()
     }
 
-    private val viewModel: FavoriteViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observe()
+        mainVm.getFavorites()
+    }
 
-        // TODO: Use the ViewModel
+    private fun observe() {
+        mainVm.favorites.observe(viewLifecycleOwner) {
+            val favoriteList = it.values.toList()
+            binding.recycleView.apply {
+                layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+                adapter = FavoriteAdapter(
+                    favoriteList,
+                    onItemClick = { onItemClick(it) },
+                    onAddToCartClick = { onAddToCartClick(it) },
+                    onDeleteClick = { onDeleteClick(it) })
+            }
+        }
+    }
+
+    fun onItemClick(favorite: Favorite) {
+
+    }
+
+    fun onAddToCartClick(favorite: Favorite) {
+
+    }
+
+    fun onDeleteClick(favorite: Favorite) {
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
